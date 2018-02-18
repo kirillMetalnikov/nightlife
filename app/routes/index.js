4db66e51fe590731067121492f586f745921b78c
kirillMetalnikov <33592457+kirillMetalnikov@users.noreply.github.com>
2018-02-18 18:01:22 +0300

    auth backend

---
+++ app/routes/index.js
@@ -0,0 +1,33 @@
+var path = process.cwd()
+
+module.exports = function(app, passport) {
+  app.route('/')
+		.get((req, res) => {
+			res.sendFile(path + '/client/public/index.html');
+		})
+
+  app.route('/auth/github/')
+    .get(passport.authenticate('github'))
+  app.route('/auth/github/callback')
+    .get(passport.authenticate('github', {
+      successRedirect: '/success',
+			failureRedirect: '/failure'
+    }))
+
+  app.route('/auth/google/')
+    .get(passport.authenticate('google', { scope: ['profile'] }))
+  app.route('/auth/google/callback')
+    .get(passport.authenticate('google', {
+      successRedirect: '/success',
+			failureRedirect: '/failure'
+    }))
+
+  app.route('/success')
+    .get( (req, res) => {
+      res.json({autht: 'success'})
+    })
+  app.route('/failure')
+    .get( (req, res) => {
+      res.json({autht: 'failure'})
+    })
+}
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 var express = require('express')
+var passport = require('passport')
+var session = require('express-session')
+//var fallback = require('express-history-api-fallback');
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
 var app = express()
+require('./app/config/passport')(passport)
+
+mongoose.connect(process.env.MONGO_URI)
+
+app.use(bodyParser.json())
+app.use('/client', express.static(process.cwd() + '/client'))
+app.use('/public', express.static(process.cwd() + '/client/public'))
+app.use(session({
+	secret: process.env.SESSION_KEY,
+	resave: false,
+	saveUninitialized: true,
+	maxAge: 60000,
+	expires: 60000
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+require('./app/routes/index.js')(app, passport)
 
 var port = process.env.PORT || 8080
 app.listen(port, () => {
