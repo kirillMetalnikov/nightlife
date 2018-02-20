17d0f510fec89f5f20acb237c9a529696166c931
kirillMetalnikov <33592457+kirillMetalnikov@users.noreply.github.com>
2018-02-20 12:56:02 +0300

    add rewiew for businesses

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -26,15 +26,16 @@ var addSubsribers = (business, userID) => {
   })
 }
 
-var getRewies = id => {
-  return new Promise( (resolve, reject) => {
-    yelp.reviews(id)
-      .then( data => {
-        resolve(data)
-      })
-      .catch (err => {
-        console.log(err)
-      })
+var addRewies = businesses => {
+  return new Promise ( (resolve, reject) => {
+    // [].map or other method is not right for async/await. Then for it need a "for"
+    return ( async () => {
+      for (let i = 0; i < businesses.length; i++) {
+        let {reviews} = JSON.parse(await yelp.reviews(businesses[i].id))
+        businesses[i].reviews = reviews[0].text
+      }
+      resolve(businesses)
+    })()
   })
 }
 
@@ -48,6 +49,7 @@ module.exports = (req, res) => {
         return {name, image_url, url, id}
       })
     })
+    .then(addRewies)
     .then(businesses => {
       var userID = req.user ? req.user._id.toString() : 'anonymous';
       return Promise.all(
